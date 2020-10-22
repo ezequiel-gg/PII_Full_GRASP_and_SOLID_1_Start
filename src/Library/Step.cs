babbe7ad85bcab07461234a3c7e3f719b2d230f4
brunoarnuti <brunoarnuti@gmail.com>
2020-10-22 12:32:02 -0300

    agrego solucion

--- src/Library/Step.cs
+++ src/Library/Step.cs
@@ -23,5 +23,20 @@ namespace Full_GRASP_And_SOLID.Library
         public int Time { get; set; }
 
         public Equipment Equipment { get; set; }
+
+        public double GetStepCost()
+        {
+            return this.GetStepProductCost() + this.GetStepEquipmentCost();
+        }
+
+        public double GetStepEquipmentCost()
+        {
+            return this.Equipment.HourlyCost * this.Time;
+        }
+
+        public double GetStepProductCost()
+        {
+            return this.Input.UnitCost * this.Quantity;
+        }
     }
 }
